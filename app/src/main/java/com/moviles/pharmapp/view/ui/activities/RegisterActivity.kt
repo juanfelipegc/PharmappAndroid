8cdac9ec9149105b152ef185e4e4e1424c068b7b
Sebastián <js.millan10@uniandes.edu.co>
2020-10-31 01:21:54 -0500

    Creatig the login

--- app/src/main/java/com/moviles/pharmapp/view/ui/activities/RegisterActivity.kt
+++ app/src/main/java/com/moviles/pharmapp/view/ui/activities/RegisterActivity.kt
@@ -1,12 +1,23 @@
 package com.moviles.pharmapp.view.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import com.moviles.pharmapp.R
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_register.*
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        setupListeners()
+    }
+
+    fun setupListeners(){
+        btnLogin.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
