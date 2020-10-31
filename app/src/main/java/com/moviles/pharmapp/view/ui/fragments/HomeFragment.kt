beb0d8ebeef39449bb152e249d9c91cd517ced29
juanfelipegc <jf.garcia@uniandes.edu.co>
2020-10-31 05:23:19 -0500

    Trying to connect screen to Firebase DB.

--- app/src/main/java/com/moviles/pharmapp/view/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/moviles/pharmapp/view/ui/fragments/HomeFragment.kt
@@ -4,17 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.moviles.pharmapp.R
+import com.moviles.pharmapp.model.Medication
+import com.moviles.pharmapp.model.User
 import com.moviles.pharmapp.view.adapter.CalendarAdapter
+import com.moviles.pharmapp.view.adapter.MedicationAdapter
+import com.moviles.pharmapp.viewmodel.HomeViewModel
+import com.moviles.pharmapp.viewmodel.MedicineBackend
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment: Fragment() {
 
     private lateinit var calendarAdpater: CalendarAdapter
+    private lateinit var viewModel: HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,5 +39,20 @@ class HomeFragment: Fragment() {
             layoutManager = linear
             adapter = calendarAdpater
         }
+
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        viewModel.refresh()
+
+        observeViewModel()
     }
+
+    fun observeViewModel() {
+        viewModel.liveUser.observe(viewLifecycleOwner, Observer<User> { user ->
+            user.let {
+                userName.text = user.name
+            }
+        })
+    }
+
+
 }
